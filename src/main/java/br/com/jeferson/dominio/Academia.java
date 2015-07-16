b5e24f09226f17d66cbc35a6e15c3abd8236fce8
JefersonQueiroga <jefersonqueiroga@gmail.com>
2015-07-16 15:11:00 -0300

    Comitar configuração hibernate

--- src/main/java/br/com/jeferson/dominio/Academia.java
+++ src/main/java/br/com/jeferson/dominio/Academia.java
@@ -1,5 +1,33 @@
 package br.com.jeferson.dominio;
 
-public class Academia {
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 
+@Entity
+public class Academia {
+	
+	@Id
+	@GeneratedValue
+	private int id;
+	
+	private String nome;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	
 }
---
+++ src/main/java/br/com/jeferson/teste/Main.java
@@ -0,0 +1,26 @@
+package br.com.jeferson.teste;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import br.com.jeferson.dominio.Academia;
+
+public class Main {
+	public static void main(String[] args) {
+		Academia academia = new Academia();
+		academia.setNome("Fit PDF");
+
+	    EntityManagerFactory factory = Persistence.
+	          createEntityManagerFactory("projeto2");
+	    EntityManager manager = factory.createEntityManager();
+
+	    manager.getTransaction().begin();    
+	    manager.persist(academia);
+	    manager.getTransaction().commit();  
+
+	    System.out.println("ID da tarefa: " + academia.getId() );
+
+	    manager.close();
+	}
+}
